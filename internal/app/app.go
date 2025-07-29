5d01489b59670da21e5177a2816e313afc9a8805
nikitalobanov12 <nikitaalobanovv@gmail.com>
2025-07-29 14:27:31 -0700

    createWorkoutHandler checkpoint

---
+++ internal/app/app.go
@@ -0,0 +1,47 @@
+package app
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/nikitalobanov12/fm_project/internal/api"
+	"github.com/nikitalobanov12/fm_project/internal/store"
+	"github.com/nikitalobanov12/fm_project/migrations"
+)
+
+type Application struct {
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
+}
+
+func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
+	}
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	workoutHandler := api.NewWorkoutHandler()
+
+	app := &Application{
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
+	}
+
+	return app, nil
+}
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
+}
